72936194a40129ac869034c34debc618f181e4ab
Mario Almandoz <marioalmandoz@MacBook-Air-de-Nekane.local>
2024-10-03 12:56:02 +0200

    part1 exercise 1.3

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,4 +1,5 @@
 const Header = (props) => {
+  console.log(props)
   return(
       <div>
         <h1>{props.course}</h1>
@@ -32,18 +33,26 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name : 'Fundamentals of React',
+    exercises : 10
+  } 
+  const part2 = {
+    name : 'Using props to pass data',
+    exercises : 7
+  }
+  const part3 = {
+    name : 'State of a component',
+    exercises : 14
+  }
 
   return (
     <div>
       <Header course = {course} />
-      <Content parte1 = {part1} number1 = {exercises1} parte2 = {part2} number2 = {exercises2} parte3 = {part3} number3 = {exercises3}/>
-      <Total total= {exercises1 + exercises2 + exercises3} />
+      <Content parte1 = {part1.name} number1 = {part1.exercises} 
+      parte2 = {part2.name} number2 = {part2.exercises} 
+      parte3 = {part3.name} number3 = {part3.exercises}/>
+      <Total total= {part1.exercises + part2.exercises + part3.exercises} />
     </div>
   )
 }

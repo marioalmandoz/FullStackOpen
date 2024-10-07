51d0870c50352472335c6b3241313383af3b8897
Mario Almandoz <marioalmandoz@Air-de-Nekane.station>
2024-10-07 10:27:12 +0200

    1.8

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -32,8 +32,6 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  
-  
 
   const header = 'give feedback'
 

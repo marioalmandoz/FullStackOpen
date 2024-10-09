993c9283b293a42fbb54f45e9a0f110ce616d54f
Mario Almandoz <marioalmandoz@MacBook-Air-de-Nekane.local>
2024-10-09 18:27:13 +0200

    2.6

---
+++ part2/AgendaTelefonica/src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handleAddPerson = (event) => {
+    event.preventDefault()
+    console.log('button clicked', event.target)
+    if(newName){
+      const newPerson = {name : newName};
+      console.log(newName)
+      setPersons(persons.concat(newPerson));
+      setNewName('');
+      console.log(newName)
+    }
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleAddPerson}>
+        <div>
+          name: <input value = {newName} onChange = {handleNameChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      
+      <div> 
+        {persons.map((person, index)=>(
+        <p key= {index}> {person.name} </p>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default App
